8966791b3d8e93fe01dc9fe7a0628f85a5176298
mohosin2126 <mohosin212@gmail.com>
2023-09-27 22:57:54 +0800

    changed the opacity of the banner photo

--- src/components/Header/Banner.jsx
+++ src/components/Header/Banner.jsx
@@ -2,7 +2,7 @@ const Banner = () => {
     return (
         <div>
             <div className="hero h-96 mt-5" style={{backgroundImage: 'url(https://i.ibb.co/PzC8fLR/Rectangle-4281.png)'}}>
-  <div className="hero-overlay bg-opacity-70 bg-white "></div>
+  <div className="hero-overlay bg-opacity-80 bg-white "></div>
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-md">
       <h1 className="mb-5 text-2xl font-black text-center text-black">I Grow By Helping People In Need</h1>
